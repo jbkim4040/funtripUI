dd7db2294f33bd5bc723b9d5b0aa742329950167
sihun93 <82190707+sihun93@users.noreply.github.com>
2023-10-20 16:54:10 +0900

    서버연결

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
 
 function App() {
+  const [msg, setMsg] = useState([]);
+  useEffect(() => {
+    fetch("/test/test")
+        .then((res) => {return res.json();})
+        .then((data) => {setMsg(data);})
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {msg.map((content, idx) => <li key={`${idx} - ${content}`}>{content}</li>)}
+        </ul>
       </header>
     </div>
   );
